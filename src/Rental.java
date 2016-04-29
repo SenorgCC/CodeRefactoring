112862bf4a0ed49fc917383e8d9b937aac199753
SenorgCC <Alex.Sinicyn@gmx.de>
2016-04-29 12:17:49 +0200

    getFrequentRenterPoints moved to Movie

--- src/Rental.java
+++ src/Rental.java
@@ -14,12 +14,8 @@ class Rental {
     double getCharge(){
         return  movie.getCharge(daysRented);
     }
-
-     int getFrequentRenterPoints() {
-
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+    int getFrequentRenterPoints() {
+        return movie.getFrequentRenterPoints(daysRented);
     }
+
 }
