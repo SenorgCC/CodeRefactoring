3f6fe3af381ee3899e2e56000a7472645759dc1e
SenorgCC <Alex.Sinicyn@gmx.de>
2016-04-29 09:55:08 +0200

    - Creating Project and Test Directory

---
+++ Test/ProgramTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+/**
+ * Created by Alex on 29.04.2016.
+ */
+public class ProgramTest {
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
+
+}
---
+++ Test/TestMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Alex on 29.04.2016.
+ */
+
+public class TestMain {
+    public static void main(string args[]){
+
+    }
+}
